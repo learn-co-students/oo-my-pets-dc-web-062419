a1eab9474cb1c4510aaa23f730ef3df2c7d9faef
claudiasolbes <cssolbes@hotmail.com>
2019-06-27 15:28:42 -0400

    first part

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+  @@all_cats = []
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all_cats << self
+  end
+  def self.all
+    @@all_cats
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
+require_relative "./cat.rb"
+require "pry"
+
 class Owner
-  # code goes here
+  attr_reader :species, :name
+  @@all_owners = []
+  def initialize(name)
+    @species = "human"
+    @name = name
+    @@all_owners << self
+  end
+  def self.all
+    @@all_owners
+  end
+  def self.count
+    @@all_owners.size
+  end
+  def self.reset_all
+    @@all_owners.clear
+  end
+  def say_species
+    "I am a #{@species}."
+  end
+  def cats
+  end
+  def count
+    @count = 0
+  end
+  def buy_cat(cat_name)
+
+  end
 end
